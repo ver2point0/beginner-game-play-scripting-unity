42d4fbe07e20f00bee5d4899190e2a7a8088a9f2
John McIntosh II <jmci10@hotmail.com>
2016-12-22 18:18:34 -0500

    add for loop

---
+++ Assets/c#/ForLoop.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoop : MonoBehaviour {
+
+	int numberOfEnemies = 5;
+	// Use this for initialization
+	void Start() {
+		for(int i = 0; i < numberOfEnemies; i++) {
+			Debug.Log("Creating enemy number: " + i);
+		}
+	}
+}
