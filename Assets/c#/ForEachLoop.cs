b2de208c0fa169993d8fe16c58e0d78aac012d40
John McIntosh II <jmci10@hotmail.com>
2016-12-22 18:24:34 -0500

    add for each loop

---
+++ Assets/c#/ForEachLoop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForEachLoop : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		string[] strings = new string[3];
+
+		strings[0] = "First";
+		strings[1] = "Second";
+		strings[2] = "Third";
+
+		foreach(string item in strings) {
+			print(item);
+		}
+	}
+}
