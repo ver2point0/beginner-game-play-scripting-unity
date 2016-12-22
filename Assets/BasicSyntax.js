95bf12d2ce42b80a02f9d13cb10f36be453bc6d8
John McIntosh II <jmci10@hotmail.com>
2016-12-22 02:08:30 -0500

    add example JavaScript syntax

---
+++ Assets/BasicSyntax.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+	Debug.Log(transform.position.x);
+	
+	if(transform.position.y <= 5f) {
+		Debug.Log("I'm about to hit the ground!");
+	}
+}
---
+++ Assets/ExampleJsSyntax.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var myInt = 5;
+
+function MyFunction(number : int) : int {
+	var ret = myInt * number;
+	return ret;
+}
