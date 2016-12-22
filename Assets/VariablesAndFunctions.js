e71aefdf3724fa88f98f3dbaff924ca65dbfc8a7
John McIntosh II <jmci10@hotmail.com>
2016-12-22 01:53:28 -0500

    add variables and functions

---
+++ Assets/VariablesAndFunctions.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var myInt : int = 10;
+
+function Start () {
+	myInt = MultiplyByTwo(myInt);
+	Debug.Log(myInt);
+}
+
+function MultiplyByTwo(number : int) : int {
+	var returnValue : int;
+	returnValue = number * 2;
+	return returnValue;
+}
