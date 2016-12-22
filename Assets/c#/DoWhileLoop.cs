a56b9aada9748cc7217c4b9dbb99ee237f9de87e
John McIntosh II <jmci10@hotmail.com>
2016-12-22 18:13:28 -0500

    add do while loop

---
+++ Assets/c#/DoWhileLoop.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoWhileLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		bool shouldContinue = false;
+		do {
+			print("Hello World!");
+		} while(shouldContinue == true);
+	}
+}
