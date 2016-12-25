36f3103d4148d1b0ec764a4ae50c15c5cd966504
John McIntosh II <jmci10@hotmail.com>
2016-12-25 03:40:05 -0500

    add awake and start

---
+++ Assets/c#/AwakeAndStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AwakeAndStart : MonoBehaviour {
+
+	void Awake() {
+		Debug.Log("Awake called");
+	}
+
+	void Start() {
+		Debug.Log("Start called");
+	}
+}
---
+++ Assets/c#/UpdateAndFixedUpdate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateAndFixedUpdate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
