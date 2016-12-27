ca77d4a408ed9598741cf4187cea67aad200fa5b
John McIntosh II <jmci10@hotmail.com>
2016-12-27 02:42:13 -0500

    add active objects

---
+++ Assets/c#/CheckState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckState : MonoBehaviour {
+
+	public GameObject myObject;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Active Self: " + myObject.activeSelf);
+		Debug.Log("Active in Hierarchy: " + myObject.activeInHierarchy);
+	}
+}
