6897576c41defbe13d83b3ad24e5bd98d7cb1914
John McIntosh II <jmci10@hotmail.com>
2017-01-03 04:19:09 -0500

    add axis raw example

---
+++ Assets/c#/AxisRawExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxisRawExample : MonoBehaviour {
+
+	public float range;
+	public GUIText textOutput;
+	
+	// Update is called once per frame
+	void Update() {
+		float h = Input.GetAxisRaw("Horizontal");
+		float xPos = h * range;
+		
+		transform.position = new Vector3(xPos, 2f, 0);
+		textOutput.text = "Value returned: " + h.ToString("F2");
+	}
+}
