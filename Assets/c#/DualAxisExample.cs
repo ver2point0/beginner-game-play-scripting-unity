77947442047e21eb21414273216a4eac9fd067c0
John McIntosh II <jmci10@hotmail.com>
2017-01-03 04:37:16 -0500

    add dual axis example

---
+++ Assets/c#/DualAxisExample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DualAxisExample : MonoBehaviour {
+
+	public float range;
+	public GUIText textOutput;
+	
+	// Update is called once per frame
+	void Update() {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		float xPos = h * range;
+		float yPos = v * range;
+		
+		transform.position = new Vector3(xPos, yPos, 0);
+		textOutput.text = "Horizonal Value returned: " + h.ToString("F2") + "\nVertical Value returned: " + v.ToString("F2");
+	}
+}
