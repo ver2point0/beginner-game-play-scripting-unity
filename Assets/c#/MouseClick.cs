7050d68a6b3b0ae2c8c4b832673c976e31f2f120
John McIntosh II <jmci10@hotmail.com>
2017-01-05 01:47:50 -0500

    add mouse click

---
+++ Assets/c#/MouseClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour {
+
+	void OnMouseDown() {
+		GetComponent<Rigidbody>().AddForce(-transform.forward * 500f);
+		GetComponent<Rigidbody>().useGravity = true;
+	}
+}
