7a56f114cbf7ffaa8b713fa961b93df3cb78a4a1
John McIntosh II <jmci10@hotmail.com>
2017-01-06 03:35:14 -0500

    add delta time to activate game object light

---
+++ Assets/c#/UsingDeltaTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UsingDeltaTime : MonoBehaviour {
+	public float speed = 8f; 
+	public float countdown = 3.0f;
+	
+	
+	void Update () {
+		countdown -= Time.deltaTime;
+		if(countdown <= 0.0f)
+			GetComponent<Light>().enabled = true;
+		
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+	}   
+}	
