450c8bb27739241ba235dec1a62e6ffce26ca97a
John McIntosh II <jmci10@hotmail.com>
2017-01-07 03:06:23 -0500

    add data type

---
+++ Assets/c#/DatatypeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DatatypeScript : MonoBehaviour {
+	
+	void Start() {
+		//value type variable
+		Vector3 pos = transform.position;
+		pos = new Vector3(0, 2, 0);
+
+		//reference type variable
+		Transform tran = transform;
+		tran.position = new Vector3(0, 2, 0);
+	}
+}
