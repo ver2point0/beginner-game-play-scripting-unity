f0b8643797e3cdfad90e409cdf2a3011746117c5
John McIntosh II <jmci10@hotmail.com>
2017-01-10 04:02:38 -0500

    add separate movement, shooting, & inventory scripts

---
+++ Assets/c#/MovementControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementControls : MonoBehaviour {
+
+	public float speed;
+	public float turnSpeed;
+	
+	// Update is called once per frame
+	void Update() {
+		Movement();
+	}
+
+	void Movement() {
+		float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+		transform.Translate(Vector3.forward * forwardMovement);
+		transform.Rotate(Vector3.up * turnMovement);
+	}
+}
