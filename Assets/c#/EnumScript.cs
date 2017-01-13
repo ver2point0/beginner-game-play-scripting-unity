e9493faa32f5de105edb24cd2e57fd2cbbdf2c9f
John McIntosh II <jmci10@hotmail.com>
2017-01-13 04:00:45 -0500

    declare and use Direction enumeration

---
+++ Assets/c#/EnumScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumScript : MonoBehaviour {
+
+	enum Direction {North, East, South, West};
+
+	void Start() {
+		Direction myDirection;
+		myDirection = Direction.North;
+		Debug.Log(myDirection);
+	}
+
+	Direction ReverseDirection(Direction dir) {
+		if(dir == Direction.North)
+			dir = Direction.South;
+		else if(dir == Direction.South)
+			dir = Direction.North;
+		else if(dir == Direction.East)
+			dir = Direction.West;
+		else if(dir == Direction.West)
+			dir = Direction.East;
+
+		return dir;
+	}
+}
