21b7e4f91fe48b7287794b0e41ac7f1c541261f2
John McIntosh II <jmci10@hotmail.com>
2017-01-13 03:44:25 -0500

    use Invoke to schedule method calls

---
+++ Assets/c#/InvokeRepeating.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvokeRepeating : MonoBehaviour {
+
+	public GameObject target;
+
+	void Start() {
+		InvokeRepeating("SpawnObject", 2, 1);
+		CancelInvoke("SpawnObject");
+	}
+
+	void SpawnObject() {
+		float x = Random.Range(-2.0f, 2.0f);
+		float z = Random.Range(-2.0f, 2.0f);
+		Instantiate(target, new Vector3(x, 2, z), Quaternion.identity);
+	}
+}
